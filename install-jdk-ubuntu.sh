a56bbb6f1b6faeec0c5a8030c91f749a4eb808e4
Naveen Katiyar <naveen140990@gmail.com>
2018-01-25 15:31:27 +0530

    fixed export command

--- install-jdk-ubuntu.sh
+++ install-jdk-ubuntu.sh
@@ -3,6 +3,7 @@ add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install oracle-java8-installer
 update-alternatives --config java
-export JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+#replace JAVA HOME location by checking from above command
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export PATH=$PATH:$JAVA_HOME/bin
 
